ed04575dcb4e22fc8485bbc8414dcb025a29228a
Sireesha-git <sireesha.koniki18@gmail.com>
2022-09-06 11:48:34 +0530

    first commit

---
+++ src/main/java/com/example/moviebookingbackend/Repository/CustomerRepo.java
@@ -0,0 +1,20 @@
+package com.example.moviebookingbackend.Repository;
+
+import com.example.moviebookingbackend.Model.Bookings;
+import com.example.moviebookingbackend.Model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CustomerRepo extends JpaRepository<Customer,Integer> {
+    public Customer findByEmailAndPassword(String email , String password);
+    public Customer findByEmail(String email);
+
+    @Query("select a from Customer a where a.customerid=?1")
+    public Customer findById(int id);
+
+    @Query ("select b from Bookings b where b.customer.customerid=:customerid")
+    List<Bookings> findCustomer (@Param("customerid") int customerid);
+}
---
+++ src/main/java/com/example/moviebookingbackend/Service/CustomerService.java
@@ -0,0 +1,39 @@
+package com.example.moviebookingbackend.Service;
+
+import com.example.moviebookingbackend.Model.Bookings;
+import com.example.moviebookingbackend.Model.Customer;
+import com.example.moviebookingbackend.Repository.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepo repository ;
+
+    public Customer addData(Customer customer){
+        return repository.save(customer);
+    }
+    public Customer saveUser(Customer user) {
+        return repository.save(user);
+    }
+
+    public Customer fetchUserByEmail(String email) {
+        return repository.findByEmail(email);
+    }
+
+    public Customer fetchUserByEmailAndPassword(String email , String password) {
+        return repository.findByEmailAndPassword(email, password);
+    }
+
+    public Customer getUser(int id){
+        return repository.findById(id);
+    }
+
+    public List<Bookings> findCustomer (int customerid){
+        return repository.findCustomer(customerid);
+    }
+}
